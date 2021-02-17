08a2682c57984019ad764328a68699d253b1d69e
biasdamiasa <bias@smktelkom-mlg.sch.id>
2021-02-17 23:01:42 +0700

    Pembagian hak akses

--- routes/api.php
+++ routes/api.php
@@ -17,28 +17,39 @@ Route::post('/register', 'UserController@register');
 Route::post('/login', 'UserController@login');
 
 Route::group(['middleware' => ['jwt.verify']], function() {
-    //KELAS
-    Route::post('/kelas', 'KelasController@store');
-    Route::get('/kelas', 'KelasController@show');
+
+    Route::group(['middleware' => ['api.superadmin']], function() {
+        //KELAS
+        Route::post('/kelas', 'KelasController@store');
+        
+        //SISWA
+        Route::post('/siswa', 'SiswaController@store');
+        Route::put('/siswa/{id}', 'SiswaController@update');
+        Route::delete('/siswa/{id}', 'SiswaController@delete');
+    });
     
-    //SISWA
-    Route::post('/siswa', 'SiswaController@store');
-    Route::get('/siswa', 'SiswaController@show');
-    Route::get('/siswa/{id}', 'SiswaController@detail');
-    Route::put('/siswa/{id}', 'SiswaController@update');
-    Route::delete('/siswa/{id}', 'SiswaController@delete');
+    Route::group(['middleware' => ['api.admin']], function() {
+        //KELAS
+        Route::get('/kelas', 'KelasController@show');
+
+        //SISWA
+        Route::get('/siswa', 'SiswaController@show');
+        Route::get('/siswa/{id}', 'SiswaController@detail');
+        
+        //BUKU
+        Route::post('/buku', 'BukuController@store');
+        Route::put('/buku/{id}', 'BukuController@update');
+        Route::delete('/buku/{id}', 'BukuController@delete');
+        
+        //TRANSAKSI
+        Route::post('/transaksi/pinjam', 'TransaksiController@tambahPinjam');
+        Route::post('/transaksi/tambahdetail/{id}', 'TransaksiController@tambahDetail');
+        Route::post('/transaksi/kembali', 'TransaksiController@tambahKembali');
+    });
     
-    //BUKU
-    Route::post('/buku', 'BukuController@store');
     Route::get('/buku', 'BukuController@show');
-    Route::get('/buku/{id}', 'BukuController@detail');
-    Route::put('/buku/{id}', 'BukuController@update');
-    Route::delete('/buku/{id}', 'BukuController@delete');
+    Route::get('/buku/{id}', 'BukuController@detail');    
     
-    //TRANSAKSI
-    Route::post('/transaksi/pinjam', 'TransaksiController@tambahPinjam');
-    Route::post('/transaksi/tambahdetail/{id}', 'TransaksiController@tambahDetail');
-    Route::post('/transaksi/kembali', 'TransaksiController@tambahKembali');
 });
 
 
